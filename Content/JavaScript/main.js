eefb767c3f55d90f93f891692648fbaa6af0ec68
Karan Fatnani <karan.fatnani@aquariansoft.in>
2018-11-21 12:43:39 +0530

    Added title computed Property + Updated the existing inStock and image data properties, to Computed properties

--- Content/JavaScript/main.js
+++ Content/JavaScript/main.js
@@ -2,28 +2,34 @@ const app = new Vue({
     el: '#app', 
     data: {     
         product: 'Shoes',
-        image: 'Content/assets/images/red-shoe.png',
-        inStock: true,
+        //image: 'Content/assets/images/red-shoe.png',
+        //inStock: true,
         inventory: 0,
         details: ['80% cotton', '20% polyster', 'Gender-neutral'],
         variants: [{
             variantId: 1111,
             variantColor: 'Red',
-            variantImage: 'Content/assets/images/red-shoe.png'
+            variantImage: 'Content/assets/images/red-shoe.png',
+            variantQuantity: 0
         },
         {
             variantId: 1112,
             variantColor: 'Blue',
-            variantImage: 'Content/assets/images/blue-shoe.png'
+            variantImage: 'Content/assets/images/blue-shoe.png',
+            variantQuantity: 10
         },
         {
             variantId: 1113,
             variantColor: 'Black',
-            variantImage: 'Content/assets/images/black-shoe.png'
+            variantImage: 'Content/assets/images/black-shoe.png',
+            variantQuantity: 4
         }],
-        cart: 0
+        cart: 0,
+        brand: 'Nike',
+        selectedVariant: 0
     },
     methods:{
+        //way 1 for writing methods
         addToCart() {
             this.cart += 1;
             if(this.cart>0){
@@ -32,6 +38,7 @@ const app = new Vue({
                 this.inStock = false;
             }
         },
+        //way 2 for writing methods
         removeFromCart: function() {
             this.cart -= 1;
             if(this.cart>0){
@@ -40,8 +47,23 @@ const app = new Vue({
                 this.inStock = false;
             }
         },
-        updateProduct(variantImage) {
-            this.image = variantImage;
+        //method with parameter
+        updateProduct(index) {
+            this.selectedVariant = index;
+            //this.image = index;
+            console.log(index);
         }
-    } 
+    }, 
+    //Computed Properties
+    computed:{
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        }
+    }
 });  
